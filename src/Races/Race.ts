e93b170bf62a1977fb2387364c9cffd869b4a002
jefersongjr <jeferson_gjr@hotmail.com>
2022-10-18 17:31:11 -0300

    initial commit

---
+++ src/Races/Race.ts
@@ -0,0 +1,23 @@
+export default abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get dexterity(): number {
+    return this._dexterity;
+  }
+
+  static createRacesInstances(): number {
+    throw new Error('not Implemented');
+  }
+  
+  public abstract maxLifePoints(): number; 
+}
