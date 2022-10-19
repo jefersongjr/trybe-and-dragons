ca95b4dff2c34f458dd64c21b1d3e4de3dabc66c
jefersongjr <jeferson_gjr@hotmail.com>
2022-10-19 14:15:07 -0300

    fix: inicializando os metodos special e cost com valor 0

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -5,10 +5,10 @@ export default abstract class Archetype {
   private _special: number;
   private _cost: number;
 
-  constructor(name: string, special: number, cost: number) {
+  constructor(name: string) {
     this._name = name;
-    this._special = special;
-    this._cost = cost;
+    this._special = 0;
+    this._cost = 0;
   }
  
   public get name(): string {
