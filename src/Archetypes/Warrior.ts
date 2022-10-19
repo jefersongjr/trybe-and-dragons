67455261fcec4304d2a05fb0a9d6d71137d8d1f3
jefersongjr <jeferson_gjr@hotmail.com>
2022-10-19 14:42:57 -0300

    feat: Instanceando a classe Warrior

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,16 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Warrior extends Archetype {
+  private _energyType: EnergyType = 'stamina';
+  private static _createdArchetypeInstances = 0;
+    
+  static createdArchetypeInstances() {
+    this._createdArchetypeInstances += 1;
+    return this._createdArchetypeInstances;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
