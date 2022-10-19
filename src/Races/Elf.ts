1a3c546d2e31d5a8a3b000a46ceef5d94d1cab46
jefersongjr <jeferson_gjr@hotmail.com>
2022-10-19 11:30:35 -0300

    feat: instanceando a classe Elf

---
+++ src/Races/Elf.ts
@@ -0,0 +1,15 @@
+import Race from './Race';
+
+export default class Orc extends Race {
+  private _maxLifePoints = 99;
+  private static _createdRacesInstances = 0;
+    
+  static createdRacesInstances() {
+    this._createdRacesInstances += 1;
+    return this._createdRacesInstances;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
